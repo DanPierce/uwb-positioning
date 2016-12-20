81d3137307336d264a1bd0a8db574f21a40acfd4
Dan Pierce <jdp0009@auburn.edu>
2016-12-20 10:41:51 -0600

    cleaned up node script

--- scripts/uwb_positioning_node.py
+++ scripts/uwb_positioning_node.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import rospy
+import sys
 import numpy as np
 # from uwb_positioning import Positioning
 from geometry_msgs.msg import PoseWithCovarianceStamped,Pose2D
@@ -7,110 +8,110 @@ from timedomain_uwb.msg import P440Range
 # from math import pi,cos,sin
 from scipy.linalg import inv,block_diag
 
-rospy.init_node('uwb_positioning_node')
+class UwbPositioningNode:
 
-print "UWB STUFF"
+  def __init__(self):
 
-# ----- Parameters
-range_topic_id = rospy.get_param('~range_topic_id','/p440_range')
-pos_topic_id = rospy.get_param('~position_topic_id','/uwb_position')
+    # ----- Parameters
+    range_topic_id = rospy.get_param('~range_topic_id','/p440_range')
+    pos_topic_id = rospy.get_param('~position_topic_id','/uwb_position')
 
-max_delay = rospy.get_param('~max_measurement_delay',0.3)
+    max_delay = rospy.get_param('~max_measurement_delay',0.3)
 
-beaconIdString = rospy.get_param('~uwb_beacon_ids','[101,102,103]')
+    beaconIdString = rospy.get_param('~uwb_beacon_ids','[101,102,103]')
 
-beaconPosXString = rospy.get_param('~uwb_beacon_positions_x','[0,1.5,3]')
-beaconPosYString = rospy.get_param('~uwb_beacon_positions_y','[0,1.5,3]')
-beaconPosZString = rospy.get_param('~uwb_beacon_positions_z',None)
+    beaconPosXString = rospy.get_param('~uwb_beacon_positions_x','[0,1.5,3]')
+    beaconPosYString = rospy.get_param('~uwb_beacon_positions_y','[0,1.5,3]')
+    beaconPosZString = rospy.get_param('~uwb_beacon_positions_z',None)
 
 
-global pos_x,pos_y
-pos_x = rospy.get_param('~pos_x_i',-0.4)
-pos_y = rospy.get_param('~pos_x_i',10.25)
+    self.beaconIdList = self.stringToList(beaconIdString,int)
+    self.beaconPosXList = self.stringToList(beaconPosXString,float)
+    self.beaconPosYList = self.stringToList(beaconPosYString,float)
+    self.beaconPosZList = self.stringToList(beaconPosZString,float)
 
-# ----- Publishers
-# pose_ext_pub = rospy.Publisher('/tennibot_fused_pose_extended', PoseWithCovarianceStamped, queue_size=1000)
-pose_pub = rospy.Publisher(pos_topic_id, Pose2D, queue_size=1000)
+    # Initial position guess
+    self.pos_x = rospy.get_param('~pos_x_i',-0.4)
+    self.pos_y = rospy.get_param('~pos_x_i',10.25)
 
-# est = Positioning() # instance of Positioning class
+    self.receive_time = np.array([0.,0.,0.])
+    self.Y = np.matrix((  (0.),(0.),(0.) )).transpose()
 
-def stringToFloatList(in_string,mapping_type):
-  out_list = None
-  if in_string:
-    tmp1 = in_string[1:(len(in_string)-1)]
-    tmp2 = tmp1.split(',')
-    out_list = map(mapping_type,tmp2)
-  return out_list
+    # ----- Publishers
+    self.pose_pub = rospy.Publisher(pos_topic_id, Pose2D, queue_size=1000)
 
-beaconIdList = stringToFloatList(beaconIdString,int)
-beaconPosXList = stringToFloatList(beaconPosXString,float)
-beaconPosYList = stringToFloatList(beaconPosYString,float)
-beaconPosZList = stringToFloatList(beaconPosZString,float)
+    # ----- Subscribers
+    uwb_sub = rospy.Subscriber('/p440_range', P440Range, self.range_callback, queue_size=1)
 
-receive_time = np.array([0.,0.,0.])
-Y = np.matrix((  (0.),(0.),(0.) )).transpose()
-# meas.b_id_pos = np.array([beaconIdList[0],beaconPosXList[0],beaconPosYList[0]],[beaconIdList[1],beaconPosXList[1],beaconPosYList[1]],[beaconIdList[2],beaconPosXList[2],beaconPosYList[2]])
 
-def indexFromBeaconId(beaconId):
-  i=0
-  for b_id in beaconIdList:
-    if (b_id==beaconId):
-      return i
-    i+=1
+  def measurement_equation(self,pos,bidx):
+    return pow(pow(pos[0,0]-self.beaconPosXList[bidx],2)+pow(pos[1,0]-self.beaconPosYList[bidx],2),0.5)
 
-# def addRangeMeasurement(rangeVal,rangeVar,beaconId,time):
-#   Y[indexFromBeaconId(beaconId)]==rangeVal
+  def range_callback(self,msg):
+    cur_time = msg.header.stamp
+    cur_range = float(msg.filteredRange)/1000
+    cur_rangeVariance = float(msg.filteredRangeErrEst)/1000
+    cur_beaconId = msg.destId
 
-def measurementEquation(pos,bidx):
-  return pow(pow(pos[0,0]-beaconPosXList[bidx],2)+pow(pos[1,0]-beaconPosYList[bidx],2),0.5)
+    self.Y[self.indexFromBeaconId(cur_beaconId)]=cur_range
+    
+    for y in self.Y:
+      if (y == 0.):
+        return
 
-def rangeCallback(msg):
-  global pos_x,pos_y
+    xhat = np.matrix((  (self.pos_x),(self.pos_y) )).transpose()
+    yhat = np.matrix((  (0.),(0.),(0.) )).transpose()
+    G = np.matrix((  (0.,0.),
+                     (0.,0.),
+                     (0.,0.) ))
 
-  cur_time = msg.header.stamp
-  cur_range = float(msg.filteredRange)/1000
-  cur_rangeVariance = float(msg.filteredRangeErrEst)/1000
-  cur_beaconId = msg.destId
+    dxhat = np.matrix((  (100.),(100.) )).transpose()
+    
+    while (sum(dxhat)>0.01):
+      # Calculate geometry matrix based on best estimate
+      for i in range(len(self.beaconPosXList)):
+        yhat[i,0] = self.measurement_equation(xhat,i)
+        G[i,0]=(xhat[0,0]-self.beaconPosXList[i])/yhat[i,0]
+        G[i,1]=(xhat[1,0]-self.beaconPosYList[i])/yhat[i,0]
+      dy = self.Y-yhat
+      dxhat = (inv(G.T*G)*G.T)*dy;
+      xhat+=dxhat
 
-  Y[indexFromBeaconId(cur_beaconId)]=cur_range
+    self.pos_x = xhat[0,0]
+    self.pos_y = xhat[1,0]
+    self.Y[0,0]=0.
+    self.Y[1,0]=0.
+    self.Y[2,0]=0.
+
+    pose_msg = Pose2D()
+    pose_msg.x = xhat[0]
+    pose_msg.y = xhat[1]
+    pose_msg.theta = 0.
+
+    self.pose_pub.publish(pose_msg)
+
+  def indexFromBeaconId(self,beaconId):
+    for i in range(len(self.beaconIdList)):
+      if (self.beaconIdList[i]==beaconId):
+        return i
+
+  def stringToList(self,in_string,mapping_type):
+    out_list = None
+    if in_string:
+      tmp1 = in_string[1:(len(in_string)-1)]
+      tmp2 = tmp1.split(',')
+      out_list = map(mapping_type,tmp2)
+    return out_list
+
+def main(args):
+  rospy.init_node('uwb_positioning_node')
+
+  node = UwbPositioningNode()
   
-  for y in Y:
-    if (y == 0.):
-      return
-
-  xhat = np.matrix((  (pos_x),(pos_y) )).transpose()
-  yhat = np.matrix((  (0.),(0.),(0.) )).transpose()
-  G = np.matrix((  (0.,0.),
-                   (0.,0.),
-                   (0.,0.) ))
-
-  dxhat = np.matrix((  (100.),(100.) )).transpose()
-  
-  while (sum(dxhat)>0.01):
-    # Calculate geometry matrix based on best estimate
-    i=0
-    for tmp in beaconPosXList:
-      yhat[i,0] = measurementEquation(xhat,i)
-      G[i,0]=(xhat[0,0]-beaconPosXList[i])/yhat[i,0]
-      G[i,1]=(xhat[1,0]-beaconPosYList[i])/yhat[i,0]
-      i+=1
-    dy = Y-yhat
-    dxhat = (inv(G.T*G)*G.T)*dy;
-    xhat+=dxhat
-
-  pos_x = xhat[0,0]
-  pos_y = xhat[1,0]
-  Y[0,0]=0.
-  Y[1,0]=0.
-  Y[2,0]=0.
-
-  pose_msg = Pose2D()
-  pose_msg.x = xhat[0]
-  pose_msg.y = xhat[1]
-  pose_msg.theta = 0.
-
-  pose_pub.publish(pose_msg)
-
-uwb_range_sub = rospy.Subscriber('/p440_range', P440Range, rangeCallback, queue_size=1000)
-
-rospy.spin()
+  try:
+    rospy.spin()
+  except KeyboardInterrupt:
+    print("Shutting down")
+ 
+if __name__ == '__main__':
+  main(sys.argv)
